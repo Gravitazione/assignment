213cd3155e526031abb4dd0ea276c5a8a5d23336
Sasipasch <sasipas.ch@chococrm.com>
2023-10-02 18:00:03 +0700

    feat: constant data

---
+++ src/constant/enums/languageOptions.ts
@@ -0,0 +1,40 @@
+import { LanguageOption } from "../models/types/LanguageOption.types";
+
+const languageOptions: LanguageOption[] = [
+    {
+        value: 'en',
+        label: 'Enlish',
+    },
+    {
+        value: 'jp',
+        label: 'Japanese',
+    },
+    {
+        value: 'ch',
+        label: 'Chinese',
+    },
+    {
+        value: 'th',
+        label: 'Thai',
+    },
+    {
+        value: 'es',
+        label: 'Espanol',
+    },
+    {
+        value: 'vn',
+        label: 'Vietnam',
+    },
+    {
+        value: 'po',
+        label: 'Polski',
+    },
+    {
+        value: 'kr',
+        label: 'Korean',
+    },
+    
+    
+]
+
+export default languageOptions;
